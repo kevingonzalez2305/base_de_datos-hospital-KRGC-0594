b98c74e57912a89f1a73e3d0976f722e7ead5ff1
KEVIN RAUL GONZALEZ CHAVEZ <a.23308051280594@cbtis128.edu.mx>
2026-05-13 09:19:51 -0600

    Update guardar_doctor.php
    links

--- guardar_doctor.php
+++ guardar_doctor.php
@@ -9,7 +9,7 @@ $sql = "INSERT INTO doctores (nombre, especialidad, ciudad)
         VALUES ('$nombre', '$especialidad', '$ciudad')";
 
 if(mysqli_query($conexion, $sql)){
-    echo "Doctor guardado con éxito. <a href='formulario.html'>Volver</a>";
+    echo "Doctor guardado con éxito. <a href='http://localhost/hospital/index.html'>Volver</a>";
 } else {
     echo "Error: " . mysqli_error($conexion);
 }
